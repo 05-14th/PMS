1e40e3e61a7e7badd9a5bcae7ab951253199f844
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2025-05-01 13:54:55 +0800

    Updated tsconfig.json

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,33 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Menu, X } from "lucide-react";
-import { motion } from "framer-motion";
 
 const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <nav className="bg-grey-400 p-4 shadow-md">
       <div className="container mx-auto flex justify-center items-center">
         <ul className="flex space-x-8">
           <li>
-            <Link to="/homepage" className="text-white hover:underline" onClick={() => setIsOpen(false)}>
+            <Link to="/homepage" className="text-white hover:underline">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/inventory" className="text-white hover:underline" onClick={() => setIsOpen(false)}>
+            <Link to="/inventory" className="text-white hover:underline">
               Inventory
             </Link>
           </li>
           <li>
-            <Link to="/sales" className="text-white hover:underline" onClick={() => setIsOpen(false)}>
+            <Link to="/sales" className="text-white hover:underline">
               Sales
             </Link>
           </li>
           <li>
-            <Link to="/users" className="text-white hover:underline" onClick={() => setIsOpen(false)}>
+            <Link to="/users" className="text-white hover:underline">
               Users
             </Link>
           </li>
           <li>
-            <Link to="/control" className="text-white hover:underline" onClick={() => setIsOpen(false)}>
+            <Link to="/control" className="text-white hover:underline">
                 Control
             </Link>
           </li>
