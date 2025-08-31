2bb64871f9ec4d1460135717409a4fcef8cb55e8
IsaacObusan <isaacjohn302003@gmail.com>
2025-08-31 15:15:02 +0800

    Organize Folders

--- src/pages/Control.tsx
+++ src/pages/Control.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ControlBody from "../components/ControlBody";
-import Feedingandwatering from "./Extra/Feedingandwatering";
+import Feedingandwatering from "./Extra/Control/Feedingandwatering";
 
 
 const SubTabsPage: React.FC = () => {
