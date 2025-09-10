4b6070954180a1f4dfa6f82dffb32a47532c207b
IsaacObusan <isaacjohn302003@gmail.com>
2025-09-10 12:19:06 +0800

    reset

--- backend/migrations/001_add_session_columns.sql
+++
@@ -1,5 +0,0 @@
--- Add session_token and last_login columns to cm_users table
-ALTER TABLE cm_users
-ADD COLUMN session_token VARCHAR(255) DEFAULT NULL,
-ADD COLUMN last_login TIMESTAMP NULL DEFAULT NULL,
-ADD UNIQUE INDEX idx_session_token (session_token);
